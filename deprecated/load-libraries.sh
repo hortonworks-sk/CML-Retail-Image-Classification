930ecc5424b3851719f31372ef9ee2171641d9c9
Steve Kiaie <skiaie@cloudera.com>
2021-08-09 22:44:11 +0000

    file cleanup

--- deprecated/load-libraries.sh
+++
@@ -1,7 +0,0 @@
-!pip3 install NumPy==1.13.1
-!pip3 install keras==2.0.6
-!pip3 install matplotlib==2.0.2
-!pip3 install pandas==0.20.3
-!pip3 install scikit-learn==0.19.1
-
-!pip3 install tensorflow==1.2.1
